68913f101e5a5ef19cba40826983e44465a91c13
VeryContent <79528558+VeryContent@users.noreply.github.com>
2021-03-02 11:30:27 +0100

    Update assistenza.js

--- src/pages/servizi/assistenza.js
+++ src/pages/servizi/assistenza.js
@@ -43,9 +43,8 @@ class IndexSopralluogo extends Component {
 
                 <div className="uk-container-large" style={{marginLeft: 'auto',marginRight: 'auto'}}><div className="uk-padding-large uk-width-1-2@l uk-flex uk-flex-middle uk-first-column"><div>
                 <h1>Assistenza post vendita</h1>
-                <p>dopo la sostituzione degli infissi: servizio ad hoc</p>
-                <p>Amiamo seguire i lavori dall’inizio alla fine e anche dopo, fornendo un servizio specifico di <b>assistenza dopo la sostituzione degli infissi</b> e non solo, a cui puoi rivolgerti in ogni momento. Il nostro team è lieto di aiutarti a chiarire i tuoi dubbi e a capire come intervenire in caso di eventuali problemi, che risolviamo in fretta. </p>
-                <h2>Assistenza </h2>
+                <p>Amiamo seguire i lavori dall’inizio alla fine e anche dopo, fornendo un servizio specifico di <b>assistenza dopo la sostituzione degli infissi</b> e non solo, a cui puoi rivolgerti in ogni momento. Il nostro team è lieto di aiutarti a chiarire i tuoi dubbi e a capire come intervenire in caso di eventuali problemi. </p>
+                <h2>Assistenza dopo la sostituzione degli infissi: servizio ad hoc</h2>
                 <p>Noi di Aprire Srl abbiamo previsto un servizio mirato all’ <b>assistenza post vendita dopo la sostituzione degli infissi</b> e per altri lavori che eseguiamo nel processo costruttivo di un’abitazione. Puoi contattarci al numero 039509538 negli orari di ufficio dal lunedì al venerdì. </p>
                 <p>Il nostro incaricato virtuale per l’<b>assistenza dopo la sostituzione degli infissi</b> è disponibile anche tramite i social media per darti un supporto completo nell’individuare il problema e le tue esigenze specifiche. Seguici sui social o contattaci. Noi ci siamo e siamo pronti ad aiutarti e a venire incontro alle tue esigenze. </p>
                 </div></div></div>
